079f0330c00785ba0d1faf925fb83b479243f418
Sil silas <silaswans@gmail.com>
2020-07-14 23:17:38 +0300

    commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style.css'
 import { FaSearch } from 'react-icons/fa';
 
-export default function Navbar() {
+export default function Navbar(props) {
+
+    const[search, setSearch] = useState(false);
+
+    const submitSearch = (e) => {
+        e.preventDefault();
+        alert('searched')
+    }
+
+    const openSearch = () => {
+        setSearch(true)
+    }
+
+    // const searchClass = search ? 'searchInput active':'searchInput';
+    const searchClass = 'searchInput'
+
+
+
     return (
         <div className='navbar'>
             <ul className='navbarMenu'>
@@ -12,8 +29,10 @@ export default function Navbar() {
                <li><a href='#'>Posts</a></li>
             </ul>
             <div className='search'>
-                <input type='text' placeholder='Search'/>
-                <FaSearch className='icon'/>
+                <form onSubmit={submitSearch}>
+                <input className={searchClass}  type='text' placeholder='Search'/>
+                <FaSearch onClick={openSearch} className='icon'/>
+                </form>
             </div>
         </div>
     )
