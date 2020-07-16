6aa1dc16c2caa49c1055b5dfb6e4f78f355aaaf7
Sil silas <silaswans@gmail.com>
2020-07-16 20:41:28 +0300

    post

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.css'
 import Card from '../UI/Card'
-
+// import blogImage from '../../Data/BlogPostImages'
 export default function BlogPost() {
     return (
         <div className='blogPostContainer'>
@@ -11,7 +11,9 @@ export default function BlogPost() {
                     <h1 className='postTitle'>Beutiful Is Always Beautiful</h1>
                     <span className='postedBy'>posted on July 21, 2020 by Silas Silikhe Blog</span>
                 </div>
-
+                <div className='postImageContainer'>
+                    <img src={require("../../Data/BlogPostImages/photofour.jpg")} alt='post image'/>
+                </div>
             </Card>
         </div>
     )
