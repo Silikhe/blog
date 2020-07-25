d8b2a5d03d04f31fd1ba31081a45aaceca6dbb28
Sil silas <silaswans@gmail.com>
2020-07-25 23:54:46 +0300

    home page

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -15,7 +15,7 @@ const Sidebar = (props) => {
     useEffect(() => {
         const posts = blogPost.data
         setPosts(posts);
-    }, posts);
+    }, [posts]);
 
     return (
         <div className='sidebarContainer'>
@@ -49,7 +49,7 @@ const Sidebar = (props) => {
                     {
                         posts.map(post => {
                             return (
-                                <NavLink to={`/post/${post.id}`}>
+                                <NavLink key={post.id} to={`/post/${post.id}`}>
                                     <div className='recentPost'>
                                         <h3>{post.blogTitle}</h3>
                                         <span>{post.postedOn}</span>
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -7,12 +7,20 @@ import Card from '../../components/UI/Card';
 const Home = props => {
 
     const galaryHeight = 450;
-    return(
-        <div className='galaryPost'>
-            <Card>
-                <section>
-                  <img src={"https://images.unsplash.com/photo-1512149673953-1e251807ec7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"} alt='sila'/>
-                </section>
+    const galaryStyle = {
+        height: galaryHeight + "px",
+        overflow: "hidden",
+        width: "100%"
+    }
+
+    return (
+        <div  >
+            <Card style={{margin: "0"}}>
+                <div style={galaryStyle} className='galaryPost'>
+                        <div>
+                            <img src={"https://images.unsplash.com/photo-1512149673953-1e251807ec7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"} alt='sila' />
+                        </div>
+                </div>
             </Card>
         </div>
     );
